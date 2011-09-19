99c08049b4469e2dac9fda6867d7d18d4a69ca3e
s.hisashi <shisashi@gmail.com>
2011-09-19 19:58:13 +0900

    adding license link of synset dictionary

--- src/net/shisashi/android/ruigomush/AboutActivity.java
+++ src/net/shisashi/android/ruigomush/AboutActivity.java
@@ -20,22 +20,31 @@ public class AboutActivity extends Activity {
         MovementMethod movementMethod = LinkMovementMethod.getInstance();
 
         {
-            TextView aboutMicahHainlineLink = (TextView) findViewById(R.id.about_micah_hainline_link);
-            aboutMicahHainlineLink.setMovementMethod(movementMethod);
+            String url = getString(R.string.wordnet_ja_url);
+            String copyright1 = getString(R.string.wordnet_ja_copyright1);
+            String copyright2 = getString(R.string.wordnet_ja_copyright2);
+            String html = String.format("<a href=\"%s\">%s<br>%s</a>", url, copyright1, copyright2);
 
+            TextView wordnetJaLink = (TextView) findViewById(R.id.wordnet_ja_link);
+            wordnetJaLink.setMovementMethod(movementMethod);
+            wordnetJaLink.setText(Html.fromHtml(html));
+        }
+
+        {
             String url = getString(R.string.micah_hainline_url);
             String html = "licenced under CC-by-SA <a href=\"" + url + "\">Micah Hainline</a>";
 
+            TextView aboutMicahHainlineLink = (TextView) findViewById(R.id.about_micah_hainline_link);
+            aboutMicahHainlineLink.setMovementMethod(movementMethod);
             aboutMicahHainlineLink.setText(Html.fromHtml(html));
         }
 
         {
-            TextView aboutStackoverflowLink = (TextView) findViewById(R.id.about_stackoverflow_link);
-            aboutStackoverflowLink.setMovementMethod(movementMethod);
-
             String url = getString(R.string.rowlayout_url);
             String html = "<a href=\"" + url + "\">java - Line-breaking widget layout <br> for Android - Stack Overflow</a>";
 
+            TextView aboutStackoverflowLink = (TextView) findViewById(R.id.about_stackoverflow_link);
+            aboutStackoverflowLink.setMovementMethod(movementMethod);
             aboutStackoverflowLink.setText(Html.fromHtml(html));
         }
     }
